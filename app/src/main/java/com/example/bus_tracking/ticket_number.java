1350d441f438736825705774b8334cdd04d58b16
daniyalqamar1007 <daniyalqamar1007@gmail.com>
2021-01-17 10:48:01 +0500

    commit

---
+++ app/src/main/java/com/example/bus_tracking/ticket_number.java
@@ -0,0 +1,14 @@
+package com.example.bus_tracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ticket_number extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_number);
+    }
+}
